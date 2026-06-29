83488aeedc1b382937d574afb59a7e105423700b
RuinedLui <luidevvvvvvvvvvv@gmail.com>
2026-06-29 14:39:04 -0600

    Add estado route to return service status

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def inicio():
         "mensaje": "API de ventas operativa"
     }
 
+@app.route("/estado")
+def estado():
+    return {
+        "mensaje": "Servicio funcionando correctamente",
+        "estado": "activo"
+    }
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
